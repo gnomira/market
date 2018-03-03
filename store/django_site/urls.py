14869d933118ee57bab765fd3c2d478794f6677a
Gnomira <gnomira@bk.ru>
2018-03-03 23:20:38 +0300

    Third_week_site

--- store/django_site/urls.py
+++ store/django_site/urls.py
@@ -1,22 +1,27 @@
 # стандартный вью для админки
 from django.contrib import admin
 # модуль Джанго для определения урлов
-from django.urls import path 
+from django.urls import path, include
 # импортируем наш файл views из products
 from products import views 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
   
 # говорим Джанго о том, что хотим отображать наш вью на главной странице
 urlpatterns = [ 
   path('', views.IndexView.as_view(),name='index'), 
   path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-  path('products/new/', views.ProductCreate.as_view(), name='product_create'),
-  path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category'),
+  #path('products/new/', views.ProductCreate.as_view(), name='product_create'),
+  path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category_detail'),
   path('products/<int:pk>/order', views.OrderFormView.as_view(), name='product_order'),
+  path('signup/', views.SignUpView.as_view(), name='signup'),
+  path('thanks/', views.Order_thanks.as_view(), name='order_thanks'),
+  path('accounts/', include('django.contrib.auth.urls')),
+  #path(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
   path('admin/', admin.site.urls), 
-    #path('', views.HomePageView.as_view(), name='home'),
-    #path('women/', views.WomenPageView.as_view(), name='women'),
-    #path('men/', views.MenPageView.as_view(), name='men'),
-    #path('children/', views.ChildrenPageView.as_view(), name='children'),
+
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-  ]
