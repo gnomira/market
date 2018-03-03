5017588b826403eef5c8386112cbffeb27f02429
Gnomira <gnomira@bk.ru>
2018-03-03 23:20:38 +0300

    Third_week_site

--- store/products/models.py
+++ store/products/models.py
@@ -6,7 +6,10 @@ from django.urls import reverse
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=5000, blank=True)
+    price= models.IntegerField(default='0',blank=True)
+    image = models.ImageField(blank=True) # указываешь upload_to='media' если только в другой папке лежать будет
     category = models.ForeignKey('Category', on_delete='CASCADE', null=True,related_name="products") 
+
     def __str__(self):
         return self.title
     def get_absolute_url(self): 
@@ -15,10 +18,13 @@ class Product(models.Model):
 class Category(models.Model): 
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=5000, blank=True)
+    
     def __str__(self):
         return self.title
 
 class Order(models.Model): 
     product = models.ForeignKey(Product, on_delete='CASCADE') 
-    customer_name = models.CharField(max_length=200)
-    customer_phone = models.CharField(max_length=200)
+    user = models.ForeignKey('auth.User', on_delete='CASCADE',blank=True,null=True)
+    customer_name = models.CharField(max_length=200,blank=False )
+    customer_phone = models.CharField(max_length=200,blank=False)
+    customer_email = models.EmailField(max_length=254,blank=False,help_text='Пример: to1@example.com')
