848c69f4fb28fd13dc4aa2515eec37456d88682b
Илья <gnomira39@gmail.com>
2018-03-06 10:35:50 +0300

    Stage 3 (#6)
    * Second_week_site
    
    * Third_week_site

--- store/products/admin.py
+++ store/products/admin.py
@@ -2,11 +2,9 @@
   # импортируем из джанго методы добавления в админку  
 from django.contrib import admin 
   # импортируем нашу модель
-from .models import Product,Category
+from .models import Product,Category,Order
   
   # говорим админке зарегисрировать нашу модель
 admin.site.register(Category)
 admin.site.register(Product)
-
-
-            
+admin.site.register(Order) 
--- store/products/views.py
+++ store/products/views.py
@@ -1,15 +1,41 @@
  # импортируем модель для CBV            
-from django.views import generic 
-
-  # импортируем нашу модель
-from .models import Product
-from .models import Category
+from django.views import generic
 from django.views.generic import TemplateView
 
+  # импортируем нашу модель
+from .models import Product,Category,Order
+#для создания пользователя
+from django.contrib.auth.forms import UserCreationForm 
+from django.urls import reverse,reverse_lazy 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 class IndexView(generic.ListView): 
-    template_name = 'products_list.html' # подключаем наш Темплейт
-    context_object_name = 'products' # под каким именем передадутся данные в Темплейт
-    model = Product # название Модели
+    template_name = 'products_list.html' 
+    context_object_name = 'products' 
+    model = Product 
+
+    # метод для добавления дополнительной информации в контекст
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        # передаем в словарь контекста список всех категорий 
+        context['categories'] = Category.objects.all()
+        return context
+    
+    def get_queryset(self):
+        products = Product.objects.all()
+        # Отбираем первые 10 статей
+        paginator = Paginator(products, 10)
+        page = self.request.GET.get('page')
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            products = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            products = paginator.page(paginator.num_pages)
+        return products
 
 class ProductDetail(generic.DetailView): 
     template_name = 'product_detail.html' 
@@ -19,16 +45,59 @@ class CategoryDetail(generic.DetailView):
     template_name = 'сategory_detail.html' 
     model = Category
 
-"""
-class HomePageView(TemplateView):
-    template_name = 'home.html'
- 
-class WomenPageView(TemplateView):
-    template_name = 'women.html'
+# форма заказа
+class OrderFormView(generic.CreateView): 
+    model = Order 
+    template_name = 'order_form.html' 
+    success_url = '/thanks/' 
+    # выведем только поля, которые нужно заполнить самому человеку
+    fields = ['customer_name', 'customer_phone','customer_email']
+   
+    def form_valid(self, form):
+        # получаем ID из ссылки и передаем в ORM для фильтрации
+        product = Product.objects.get(id=self.kwargs['pk']) 
+        # передаем в поле товара нашей формы отфильтрованный товар
+        user = self.request.user 
+        if  self.request.user.is_authenticated:
+            form.instance.user=user
+        form.instance.product = product 
+        # super — перезагружает форму, нужен для работы
+        return super().form_valid(form)
 
-class MenPageView(TemplateView):
-    template_name = 'men.html'
+#для создания пользователя
+class SignUpView(generic.CreateView): 
+    form_class = UserCreationForm 
+    success_url = reverse_lazy('login') 
+    template_name = 'signup.html'
 
-class ChildrenPageView(TemplateView):
-    template_name = 'children.html'
+
+class Order_thanks(TemplateView): 
+    template_name = 'order_thanks.html' 
+    # метод для добавления дополнительной информации в контекст
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        # передаем в словарь контекста список всех категорий 
+        context['categories'] = Category.objects.all()
+        return context
+    
+
+
+""""
+ # наш секретный Вью
+class SecretAdminView(UserPassesTestMixin, generic.TemplateView):
+    # секретное содержимое
+    template_name = 'memes.html'
+
+    # проверяем условие, если пользователь — админ, то вернет True и пустит пользователя
+    def test_func(self): 
+        return self.request.user.is_superuser
+
+# форма создания продукта           
+class ProductCreate(generic.CreateView): 
+    model = Product 
+     # название нашего шаблона с формой
+    template_name = 'product_new.html' 
+    # какие поля будут в форме 
+    fields = '__all__'
+
 """
